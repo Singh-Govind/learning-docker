bbe84e7c06421e7aa21b3df8d7c33e646c010572
Govind Kumar Singh <55323661+Singh-Govind@users.noreply.github.com>
2024-09-20 13:17:35 +0530

    All the files of backend and frontend for testing

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const {hostname} = require("node:os");
+require("dotenv").config();
+
+// this was just for testing nginx load balancer and docker thingy
+
+const User = makeSchema();
+
+function makeSchema() {
+    const userSchema = new mongoose.Schema({
+        user: {type: String},
+        email: {type: String}
+    });
+
+    const User = mongoose.model('user', userSchema, 'user');
+
+    return User;
+}
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({msg: `welcome to the jungle, id: ${hostname()}`});
+});
+
+app.get("/:usern", async (req, res) => {
+    const usern = req.params.usern;
+    try {
+        const userC = await User.findOne({user: usern});
+
+        res.json({msg: "user is", userC});
+    } catch (e) {
+        res.status(500).json({msg: "something went wrong", err: e.message});
+    }
+});
+
+app.post("/make-user", async (req, res) => {
+    const {user, email} = req.body;
+    try {
+        const userC = await User.create({user, email});
+
+        res.json({msg: 'user created', userC});
+
+    } catch (e) {
+        res.status(500).json({msg: "something went wrong", err: e.message});
+    }
+});
+
+app.get("/a/hey-there", (req, res) => {
+    res.json({msg: "should receive hey there"});
+})
+
+app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("server started at port 3000");
+    } catch (e) {
+        console.log("error", e.message);
+    }
+});
